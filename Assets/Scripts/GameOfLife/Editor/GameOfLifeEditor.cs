d7dc9da9677fa07e94a936b8f729ee70e360e2e6
JLPM22 <joseluispm2@gmail.com>
2020-09-12 21:51:55 +0200

    setup

--- Assets/Scripts/GameOfLife/Editor/GameOfLifeEditor.cs
+++ Assets/Scripts/GameOfLife/Editor/GameOfLifeEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 [CustomEditor(typeof(GameOfLife))]
 public class GameOfLifeEditor : Editor
@@ -10,6 +11,37 @@ public class GameOfLifeEditor : Editor
     {
         base.OnInspectorGUI();
 
+        GameOfLife gameOfLife = (GameOfLife)target;
 
+        EditorGUILayout.Space();
+
+        // Buttons
+        GUILayout.BeginHorizontal();
+        EditorGUI.BeginDisabledGroup(gameOfLife.Paused);
+        if (GUILayout.Button("Resume"))
+        {
+            gameOfLife.Resume();
+        }
+        EditorGUI.EndDisabledGroup();
+        GUILayout.Space(25);
+        EditorGUI.BeginDisabledGroup(!gameOfLife.Paused);
+        if (GUILayout.Button("Pause"))
+        {
+            gameOfLife.Pause();
+        }
+        EditorGUI.EndDisabledGroup();
+        GUILayout.Space(25);
+        if (GUILayout.Button("Restart"))
+        {
+            gameOfLife.Restart();
+        }
+        GUILayout.EndHorizontal();
+
+        // Save Changes
+        if (GUI.changed && !EditorApplication.isPlaying)
+        {
+            EditorUtility.SetDirty(gameOfLife);
+            EditorSceneManager.MarkSceneDirty(gameOfLife.gameObject.scene);
+        }
     }
 }
--- Assets/Scripts/GameOfLife/GameOfLife.cs
+++ Assets/Scripts/GameOfLife/GameOfLife.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class GameOfLife : MonoBehaviour
 {
-    private Image Image;
+    public Camera Camera;
+    public Vector2Int Resolution;
+
+    private MeshRenderer Quad;
     private Texture2D Texture;
     private Color32[] Colors;
-    private bool Paused;
-
-    private void Awake()
-    {
-        Image = GetComponent<Image>();
-        Paused = true;
-        StartCoroutine(UpdateCor());
-    }
+    public bool Paused { get; private set; }
 
     private IEnumerator UpdateCor()
     {
@@ -30,13 +27,22 @@ public class GameOfLife : MonoBehaviour
         }
     }
 
-    public void Restart(Vector2Int resolution)
+    public void Restart()
     {
+        Quad = GetComponent<MeshRenderer>();
         Paused = true;
-        Colors = new Color32[resolution.x * resolution.y];
-        Texture = new Texture2D(resolution.x, resolution.y);
-        FillInitValues(resolution);
+        Colors = new Color32[Resolution.x * Resolution.y];
+        Texture = new Texture2D(Resolution.x, Resolution.y);
+        Quad.sharedMaterial.SetTexture("_MainTex", Texture);
+        Quad.transform.localScale = new Vector3(Resolution.x, Resolution.y, 1.0f);
+
+        PositionCamera();
+
+        FillInitValues();
         UpdateColors();
+
+        StopAllCoroutines();
+        StartCoroutine(UpdateCor());
     }
 
     public void Pause()
@@ -49,13 +55,13 @@ public class GameOfLife : MonoBehaviour
         Paused = false;
     }
 
-    private void FillInitValues(Vector2Int resolution)
+    private void FillInitValues()
     {
-        for (int y = 0; y < resolution.y; y++)
+        for (int y = 0; y < Resolution.y; y++)
         {
-            for (int x = 0; x < resolution.x; x++)
+            for (int x = 0; x < Resolution.x; x++)
             {
-                Colors[y * resolution.x + x] = new Color32(0, 0, 0, 255);
+                Colors[y * Resolution.x + x] = new Color32(0, 0, 0, 255);
             }
         }
     }
@@ -69,5 +75,12 @@ public class GameOfLife : MonoBehaviour
     private void UpdateState()
     {
 
+    }
+
+    private void PositionCamera()
+    {
+        Camera.aspect = (float)Resolution.x / Resolution.y;
+        float distanceFromQuad = (Resolution.y * 0.5f) / Mathf.Tan(Camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        Camera.transform.position = Quad.transform.position + Vector3.back * distanceFromQuad;
     }
 }
